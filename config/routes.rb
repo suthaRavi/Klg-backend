46109bdbd8c8cd78ed805acbabf06f8cbc000aac
sutharavi <ravivendan@gmail.com>
2017-11-16 22:05:51 -0500

    status added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :statuses
   resources :fulltimes
   resources :roles
   resources :departments
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112033140) do
+ActiveRecord::Schema.define(version: 20171112232521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 20171112033140) do
   create_table "roles", force: :cascade do |t|
     t.string   "name"
     t.integer  "serial"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.integer  "value"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
