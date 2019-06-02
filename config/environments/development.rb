f41cb4e3cef2c46dee52d43edda63f80f1ef50cd
sutharavi <ravivendan@gmail.com>
2019-06-02 14:56:43 -0400

    Fergot Password is set

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { :host => "localhost:4200" }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -56,7 +56,7 @@ Rails.application.configure do
   :port => 587,
   :domain => "gmail.com",
   :user_name => "ravivendan@gmail.com",
-  :password => "vendan1sutha",
+  :password => "vendan2sutha",
   :authentication => 'plain',
   :enable_starttls_auto => true
 }
